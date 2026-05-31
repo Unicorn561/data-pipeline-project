5ad7d894d10121e53568a650fb138fdb1e249ac8
Bhavya <bhavya.bagilvaidya@gmail.com>
2026-05-31 20:11:47 +0530

    fix: auto-format terraform files

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,11 @@
 variable "location" {
-    description = "The Azure Region to deploy resources"
-    type = string
-    default = "Central India"
+  description = "The Azure Region to deploy resources"
+  type        = string
+  default     = "Central India"
 }
 
 variable "cluster_name" {
-    description = "The name of the AKS cluster"
-    type        = string
-    default     = "data-pipeline-aks"
+  description = "The name of the AKS cluster"
+  type        = string
+  default     = "data-pipeline-aks"
 }
